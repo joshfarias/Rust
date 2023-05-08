ae81cb63c06b11ecdeab1d5cf6c329007a463229
Josh Farias <joshfarias.github@gmail.com>
2023-05-07 20:27:16 -0400

    Hello World

---
+++ src/HelloWorld.rs
@@ -0,0 +1,17 @@
+/*************************************
+|
+|   Program Name: Hello World.rs
+|
+|   Description: Hello World output
+|
+|   Language: Rust
+|
+|   Date: 4/28/23
+|
+|   Author Josh Farias
+|
+*************************************/
+
+fn main() {
+    println!("Hello, world!");
+}
